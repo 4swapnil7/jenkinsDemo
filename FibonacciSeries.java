1dc2c257cf0fa4248916e8abd1ebfb3691e11cee
4swapnil7 <83705166+4swapnil7@users.noreply.github.com>
2021-09-28 14:55:40 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -2,16 +2,8 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
 	public static void main(String[] args) {
-		System.out.println("Enter limit to find finionnaci series: ");
-		Scanner sc=new Scanner (System.in);
-		int n=sc.nextInt();
-		int a=0, b=1 , c ,i;
-		System.out.println("Fibionaaci series is "+a+"\n"+b);
-		for ( i=2 ; i<= n; i++) {
-			c=a+b;
-			System.out.println("\n"+c);
-			a=b;
-			b=c;
+		System.out.println("Hi SWAPNIL ");
+		
 		}
 	}
 }
